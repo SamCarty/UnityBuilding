ba9e82cc24163e2dc2fa42814674ef6cdae1e44f
Sam Carty <samcarty311@gmail.com>
2020-04-01 15:10:32 +0100

    added buttons to build and demolish

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using Debug = System.Diagnostics.Debug;
 
 public class MouseController : MonoBehaviour {
     public GameObject cursorPrefab;
+
+    Tile.TileType buildModeTile = Tile.TileType.Floor;
 
     Camera camera;
     Vector3 lastFramePos;
@@ -72,26 +75,31 @@ public class MouseController : MonoBehaviour {
         dragPreviewObjects.ForEach(obj => SimplePool.Despawn(obj));
         dragPreviewObjects.Clear();
         
+
         // While mouse button is held down, display preview of the drag area.
         if (Input.GetMouseButton(0)) {
             for (int x = startX; x <= endX; x++) {
                 for (int y = startY; y <= endY; y++) {
-                    Tile tile = WorldController.Instance.World.GetTileAt(x, y);
-                    if (tile != null) {
-                        GameObject gameObject = SimplePool.Spawn(cursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
-                        dragPreviewObjects.Add(gameObject);
+                    if (x >= 0 && y >= 0) { // Checks tile is in range.
+                        Tile tile = WorldController.Instance.World.GetTileAt(x, y);
+                        if (tile != null) {
+                            GameObject gameObject =
+                                SimplePool.Spawn(cursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                            dragPreviewObjects.Add(gameObject);
+                        }
                     }
                 }
             }
         }
-
         // End left button drag
         if (Input.GetMouseButtonUp(0)) {
             // Loop through all the tiles in the selection and change their type.
             for (int x = startX; x <= endX; x++) {
                 for (int y = startY; y <= endY; y++) {
-                    Tile tile = WorldController.Instance.World.GetTileAt(x, y);
-                    if (tile != null) tile.Type = Tile.TileType.Floor;
+                    if (x >= 0 && y >= 0) { // Checks tile is in range.
+                        Tile tile = WorldController.Instance.World.GetTileAt(x, y);
+                        if (tile != null) tile.Type = buildModeTile;
+                    }
                 }
             }
         }
@@ -107,5 +115,13 @@ public class MouseController : MonoBehaviour {
         camera.orthographicSize -= camera.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
 
         camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, 3f, 30f);
+    }
+
+    public void SetModeBuildFoundation() {
+        buildModeTile = Tile.TileType.Floor;
+    }
+
+    public void SetModeBulldoze() {
+        buildModeTile = Tile.TileType.Empty;
     }
 }
---
+++ Assets/UI/AutomaticButtonSizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticButtonSizer : MonoBehaviour {
+
+    public float childWidth = 35f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        ResizeButtons();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void ResizeButtons() {
+        Debug.Log("Resizing child buttons");
+        var sizeDelta = GetComponent<RectTransform>().sizeDelta;
+        sizeDelta.x = transform.childCount * childWidth;
+        
+        GetComponent<RectTransform>().sizeDelta = sizeDelta;
+    }
+}
